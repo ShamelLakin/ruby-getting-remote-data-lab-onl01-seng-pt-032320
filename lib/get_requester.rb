aa14c866aba8617e3a5f204a96c1376efc7f61e7
Shamel Lakin <christlife4u@gmail.com>
2020-04-29 18:18:07 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,10 +17,11 @@ class GetRequester
   
   def parse_json
     requests = JSON.parse(self.get_response_body)
-    requests.collect do |req|
-    req["results"]
-    binding.pry
-    end
+    requests
+    # .collect do |req|
+    # req["results"]
+    # binding.pry
+    # end
   end
 end 
 
