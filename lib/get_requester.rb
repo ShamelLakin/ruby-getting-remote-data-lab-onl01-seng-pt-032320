e9f8161b6f5be139327bdc4bc83d40ad8fa262a8
Shamel Lakin <christlife4u@gmail.com>
2020-04-29 17:43:35 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,4 +7,7 @@ class GetRequester
   def initialize(url)
     @url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
   end 
+  
+  def get_response_body
+  end 
 end 
